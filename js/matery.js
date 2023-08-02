deb6190de512fc99ef3efdc2cf1dbb311a1d9d38
llllz <2428170173@qq.com>
2023-08-03 07:25:55 +0800

    Site updated: 2023-08-03 07:25:55

--- js/matery.js
+++ js/matery.js
@@ -176,18 +176,18 @@ $(function () {
     $('.tooltipped').tooltip();
 });
 
-// 深色模式设置
-// function switchNightMode() {
-//     var body = document.body;
-//     if(body.classList.contains('dark')){
-//     document.body.classList.remove('dark');
-//     localStorage.setItem('dark','0');
-//     $('#nightMode').removeClass("fa-lightbulb").addClass("fa-lightbulb");
-//     return;
-//     } else {
-//     document.body.classList.add('dark');
-//     localStorage.setItem('dark','1');
-//     $('#nightMode').removeClass("fa-lightbulb").addClass("fa-lightbulb");
-//     return;
-//     }
-// }
+深色模式设置
+function switchNightMode() {
+    var body = document.body;
+    if(body.classList.contains('dark')){
+    document.body.classList.remove('dark');
+    localStorage.setItem('dark','0');
+    $('#nightMode').removeClass("fa-lightbulb").addClass("fa-lightbulb");
+    return;
+    } else {
+    document.body.classList.add('dark');
+    localStorage.setItem('dark','1');
+    $('#nightMode').removeClass("fa-lightbulb").addClass("fa-lightbulb");
+    return;
+    }
+}
